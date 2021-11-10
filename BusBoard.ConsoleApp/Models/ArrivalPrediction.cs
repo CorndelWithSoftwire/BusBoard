99e21bf115c7336e2c56e3849ce0f9907c4df7e5
Tim Leach <timleach635@gmail.com>
2021-11-10 16:35:50 +0000

    Add part 1 code

---
+++ BusBoard.ConsoleApp/Models/ArrivalPrediction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BusBoard.ConsoleApp.Models
+{
+    public class ArrivalPrediction
+    {
+        public string VehicleId { get; set; }
+        public string StationName { get; set; }
+        public string LineId { get; set; }
+        public string LineName { get; set; }
+        public string PlatformName { get; set; }
+        public string DestinationName { get; set; }
+        public int TimeToStation { get; set; }  // predicted; given in seconds
+        public DateTime ExpectedArrival { get; set; }  // predicted
+
+        public override string ToString()
+        {
+            return String.Format("Expected {0:t}: Bus {1} to {2}", ExpectedArrival, LineId, DestinationName);
+        }
+    }
+}
