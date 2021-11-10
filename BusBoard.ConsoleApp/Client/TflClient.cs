7b59ca7124395f6c503d325c0d0020bac8032654
Tim Leach <timleach635@gmail.com>
2021-11-10 16:41:32 +0000

    Commonise client method

---
+++ BusBoard.ConsoleApp/Client/TflClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using BusBoard.ConsoleApp.Models;
+using RestSharp;
+
+namespace BusBoard.ConsoleApp.Client
+{
+    public class TflClient
+    {
+        private const string AppKey = "4a212c35b632ba9d2f43c5e1fcb7c3e6";
+        private RestClient _client;
+
+        public TflClient()
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            _client = new RestClient("https://api.tfl.gov.uk");
+        }
+        
+        public List<ArrivalPrediction> GetArrivalPredictions(string stopCode)
+        {
+            var request = new RestRequest(
+                String.Format("StopPoint/{0}/Arrivals?app_key={1}", stopCode, AppKey)
+            );
+            var response = _client.Get<List<ArrivalPrediction>>(request);
+            return response.Data;
+        }
+    }
+}
--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -1,34 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using BusBoard.ConsoleApp.Models;
-using RestSharp;
+using BusBoard.ConsoleApp.Client;
 
 namespace BusBoard.ConsoleApp
 {
   class Program
   {
-    private const string AppKey = "4a212c35b632ba9d2f43c5e1fcb7c3e6";
-    
     static void Main(string[] args)
     {
-      ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+      var client = new TflClient();
       
-      var client = new RestClient("https://api.tfl.gov.uk");
-
-      var request = new RestRequest(
-        String.Format("StopPoint/490008660N/Arrivals?app_key={0}", AppKey)
-      );
-
-      var response = client.Get<List<ArrivalPrediction>>(request);
-
-      var nextArrivals = response.Data
+      var nextArrivals = client.GetArrivalPredictions(args[0])
         .OrderBy(a => a.TimeToStation)
         .Take(5)
         .ToList();
 
-      Console.Out.WriteLine("Next {0} arrivals:", nextArrivals.Count());
+      Console.Out.WriteLine("Next {0} arrivals at stop {1}:", nextArrivals.Count, args[0]);
       foreach (var arrival in nextArrivals)
       {
         Console.Out.WriteLine(arrival);
