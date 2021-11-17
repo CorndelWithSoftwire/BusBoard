83ed9601bbe01752066e1e1c3f50a5e6e01c23de
Tim Leach <timleach635@gmail.com>
2021-11-17 11:54:34 +0000

    Deleted a bunch of text

--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -3,32 +3,7 @@ using System.Linq;
 using BusBoard.ConsoleApp.Client;
 
 namespace BusBoard.ConsoleApp
-{
-  class Program
-  {
-    static void Main(string[] args)
-    {
-      var tflClient = new TflClient();
-      var postcodeClient = new PostcodeClient();
-
-      switch (args[0])
-      {
-        case "--stop-code":
-          var nextArrivals = tflClient.GetArrivalPredictions(args[1])
-            .OrderBy(a => a.TimeToStation)
-            .Take(5)
-            .ToList();
-
-          Console.Out.WriteLine("Next {0} arrivals at stop {1}:", nextArrivals.Count, args[1]);
-          foreach (var arrival in nextArrivals)
-          {
-            Console.Out.WriteLine(arrival);
-          }
-          break;
-        case "--postcode":
-          var postcodeData = postcodeClient.GetPostcodeData(args[1]);
-          var stopPoints = tflClient
-            .GetBusStopPointsWithin(postcodeData.Latitude, postcodeData.Longitude, 500)
+00)
             .OrderBy(sp => sp.Distance)
             .Take(2)
             .ToList();
