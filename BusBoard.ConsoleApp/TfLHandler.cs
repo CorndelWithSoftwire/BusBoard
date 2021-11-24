447b057422bc8d183d23470506fc571cfcf907d1
jknights2000 <56910576+jknights2000@users.noreply.github.com>
2021-11-24 12:44:45 +0000

    class set up and start on tflhander and program

---
+++ BusBoard.ConsoleApp/TfLHandler.cs
@@ -0,0 +1,25 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusBoard.ConsoleApp
+{
+    class TfLHandler
+    {
+        public void getArrivals(string code)
+        {
+            var client = new RestClient("https://api.tfl.gov.uk/");
+            var request = new RestRequest("StopPoint/"+code+"/Arrivals",Method.GET, DataFormat.Json);
+            var reponse = client.Execute(request);
+            if(reponse.Content == null)
+            {
+                throw new Exception("got here");
+            }
+            Console.WriteLine(reponse.ContentType);
+            Console.WriteLine(reponse.Content);
+        }
+    }
+}
