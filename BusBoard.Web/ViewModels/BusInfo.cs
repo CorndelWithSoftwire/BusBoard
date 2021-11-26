df602d8394d73e88999a0f3bec3f59ff74023928
ChrisSweeneyIRIS <93913592+ChrisSweeneyIRIS@users.noreply.github.com>
2021-11-26 14:18:48 +0000

    List & Dictionaries Midway

--- BusBoard.Web/ViewModels/BusInfo.cs
+++ BusBoard.Web/ViewModels/BusInfo.cs
@@ -1,13 +1,19 @@
-﻿namespace BusBoard.Web.ViewModels
+﻿using BusBoard.Api;
+
+namespace BusBoard.Web.ViewModels
 {
   public class BusInfo
   {
     public BusInfo(string postCode)
     {
       PostCode = postCode;
+            TfLHandler t = new TfLHandler();
+            PostCodeHandler p = new PostCodeHandler();
+            t.getStopsWithinRadius(p.GetLatLong(postCode));
     }
 
     public string PostCode { get; set; }
-
+    
+    
   }
 }
